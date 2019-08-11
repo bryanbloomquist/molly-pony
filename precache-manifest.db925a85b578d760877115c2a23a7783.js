7e059fc74f52c47eae85dda870355f12cd82a03f
bloomy215 <bmbloomquist@gmail.com>
2019-08-10 23:05:28 -0500

    Updates

---
+++ precache-manifest.db925a85b578d760877115c2a23a7783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4f4ed0f51fc9507d64af5bf739072f",
+    "url": "/molly-pony/index.html"
+  },
+  {
+    "revision": "ceb177db93245a0a6475",
+    "url": "/molly-pony/static/css/main.e599e8ae.chunk.css"
+  },
+  {
+    "revision": "0e967811e48c8140d498",
+    "url": "/molly-pony/static/js/2.16029949.chunk.js"
+  },
+  {
+    "revision": "ceb177db93245a0a6475",
+    "url": "/molly-pony/static/js/main.58442596.chunk.js"
+  },
+  {
+    "revision": "821a2a990b0e6734266d",
+    "url": "/molly-pony/static/js/runtime~main.3ac24fc8.js"
+  },
+  {
+    "revision": "77072ab90b7962730ccf9ca589fef6a9",
+    "url": "/molly-pony/static/media/PonyvilleBackground.77072ab9.png"
+  }
+]);
