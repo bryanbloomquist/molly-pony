5a859800c22fb659da05ec5f7b5fb7564079397e
bloomy215 <bmbloomquist@gmail.com>
2019-09-04 23:51:26 -0500

    beginning work on difficulty selection

--- src/Components/EasyGame.js
+++ src/Components/EasyGame.js
@@ -2,33 +2,25 @@ import React, { Component } from 'react';
 import { Container } from "react-bootstrap";
 import CutieMarksJSON from "./CutieMarks.json";
 import MyLittlePoniesEasyJSON from "./myLittlePoniesEasy.json";
-import MyLittlePoniesNormalJSON from "./myLittlePoniesNormal.json";
-import MyLittlePoniesHardJSON from "./myLittlePoniesHard.json";
-import CutieMark from "./Components/GameArea/CutieMark.js";
-import GameArea from "./Components/GameArea/GameArea";
-import MyLittlePony from "./Components/PonyArea/MyLittlePony";
-import PonyArea from "./Components/PonyArea/PonyArea";
-import Billboard from "./Components/Billboard";
-import LoginArea from "./Components/LoginArea";
 import Scoreboard from "./Components/Scoreboard";
+import Billboard from "./Components/Billboard";
+import GameArea from "./Components/GameArea/GameArea";
+import CutieMark from "./Components/GameArea/CutieMark.js";
+import PonyArea from "./Components/PonyArea/PonyArea";
+import MyLittlePony from "./Components/PonyArea/MyLittlePony";
 import './App.css';
 
 class App extends Component {
 
   state = {
-    playerName: 0,
     playerWins: 0,
     playerLosses: 0,
     targetScore: 0,
     playerScore: 0,
     totalClicks: 0,
-    winCondition: 0,
-    difficulty: 0,
     cutieMarks: CutieMarksJSON,
-    myLittlePoniesEasy: MyLittlePoniesEasyJSON,
-    myLittlePoniesNormal: MyLittlePoniesEasyJSON.concat( MyLittlePoniesNormalJSON ),
-    myLittlePoniesHard: MyLittlePoniesEasyJSON.concat( MyLittlePoniesNormalJSON, MyLittlePoniesHardJSON ),
-    display: "Please enter your name and select a difficulty to begin..."
+    myLittlePonies: MyLittlePoniesEasyJSON,
+    display: "Match the Target Score by clicking on the Cutie Marks, each Cutie Mark has a hidden value."
   }
 
   // function to shuffle array
@@ -54,35 +46,14 @@ class App extends Component {
   componentDidMount() {
     this.generateTargetScore();
     this.shuffleArray( this.state.cutieMarks );
-    this.shuffleArray( this.state.myLittlePoniesEasy );
-  }
-
-  // render play area
-  renderPlayArea() {
-    if ( this.state.playerName === 0 ) {
-      return <LoginArea/>
-    } else if ( this.state.playerName !== 0 && this.state.difficulty !== 0 ) {
-      return (
-        <GameArea>
-          { this.state.cutieMarks.slice( 0, 4 ).map(( cutieMark ) => (
-            <CutieMark
-              clickMark = { this.clickMark }
-              key = { cutieMark.id }
-              id = { cutieMark.id }
-              name = { cutieMark.name }
-              image = { cutieMark.image }
-            />
-          ))}
-        </GameArea>
-      )
-    }
+    this.shuffleArray( this.state.myLittlePonies );
   }
 
   // reveals a pony after every fifth win
   selectAPony = ( x ) => {
-    let mlpCopy = JSON.parse( JSON.stringify( this.state.myLittlePoniesEasy ))
+    let mlpCopy = JSON.parse( JSON.stringify( this.state.myLittlePonies ))
     mlpCopy[ x ].unlocked = 1
-    this.setState({ myLittlePoniesEasy: mlpCopy })
+    this.setState({ myLittlePonies: mlpCopy })
   }
 
   // if Player Score = Target Score
@@ -95,7 +66,7 @@ class App extends Component {
       text = "Amazing job! You found all the My Little Ponies! It only took you " + this.state.totalClicks + " clicks, that is awesome!";
     } else if ( wins < 12 ) {
       let x = this.state.playerWins;
-      text = "Good job! You found " + this.state.myLittlePoniesEasy[ x ].name + "!";
+      text = "Good job! You found " + this.state.myLittlePonies[ x ].name + "!";
       this.selectAPony( x );
     }
     this.setState({
@@ -144,7 +115,17 @@ class App extends Component {
         <Billboard 
           display = { this.state.display }
         />
-        { this.renderPlayArea() }
+        <GameArea>
+          { this.state.cutieMarks.slice( 0, 4 ).map(( cutieMark ) => (
+            <CutieMark
+              clickMark = { this.clickMark }
+              key = { cutieMark.id }
+              id = { cutieMark.id }
+              name = { cutieMark.name }
+              image = { cutieMark.image }
+            />
+          ))}
+        </GameArea>
         <Scoreboard 
           targetScore = { this.state.targetScore }
           playerScore = { this.state.playerScore }
@@ -153,7 +134,7 @@ class App extends Component {
           totalClicks = { this.state.totalClicks }
         />
         <PonyArea>
-          { this.state.myLittlePoniesEasy.map(( myLittlePony ) => (
+          { this.state.myLittlePonies.map(( myLittlePony ) => (
             <MyLittlePony
               key = { myLittlePony.id }
               name = { myLittlePony.name }
